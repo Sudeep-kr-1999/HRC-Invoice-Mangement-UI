b20becca093aa15e671c7e42e8d293ea281f877f
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2022-04-07 17:36:03 +0530

    Adding Functionality Done

--- src/components/DialogComponent.js
+++ src/components/DialogComponent.js
@@ -9,7 +9,30 @@ import { DialogDisplayContext } from "./StateProvider";
 import API from "../axios";
 
 function DialogComponent({ dialogName, dialogElement }) {
-  const [apiBody, setapiBody] = useState({});
+  let isBusinessCodeExist;
+  let isCustomerNumberExist;
+  let isInvoiceIdorDocIdExist;
+  const [apiBody, setapiBody] = useState({
+    business_code: "U001",
+    cust_number: "200704045",
+    clear_date: "2022-03-13",
+    business_year: "2022",
+    doc_id: "1938001531",
+    posting_date: "2022-03-13",
+    document_create_date: "2022-03-13",
+    due_in_date: "2022-03-13",
+    invoice_currency: "USD",
+    document_type: "RV",
+    posting_id: "1",
+    total_open_amount: "1998.64",
+    baseline_create_date: "2022-03-13",
+    cust_payment_terms: "NAA8",
+    invoice_id: "1930702251",
+    customer_number: "200704045",
+    sl_no: "48580",
+    new_invoice_currency: "INR",
+    new_cust_payment_terms: "ABCD",
+  });
   console.log("dialogcomponent");
   const {
     dialogDisplay,
@@ -38,13 +61,140 @@ function DialogComponent({ dialogName, dialogElement }) {
     actionType = "DELETE";
   }
 
+  const formatDate = useCallback(
+    (date) => {
+      var d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      return [year, month, day].join("-");
+    },
+    [
+      apiBody.clear_date,
+      apiBody.posting_date,
+      apiBody.document_create_date,
+      apiBody.baseline_create_date,
+    ]
+  );
+
   const cancelFunction = () => {
     changeDialogDisplay("none");
     changeDialogBoxPassingData([]);
   };
 
-  const addFunction = useCallback(() => {
+  const checkBusinessCode = useCallback(
+    async ({ business_code }) => {
+      try {
+        const response = await API.get(
+          `CheckBusiness?business_code="${business_code}"`
+        );
+        if (response.status === 200) {
+          isBusinessCodeExist = response.data.isBusinessExists;
+        }
+      } catch (error) {
+        console.log("Some Error Occured");
+      }
+    },
+    [apiBody.business_code]
+  );
+  const checkCustomerNumber = useCallback(
+    async ({ cust_number }) => {
+      try {
+        const response = await API.get(
+          `CheckCustomer?cust_number="${cust_number}"`
+        );
+        if (response.status === 200) {
+          isCustomerNumberExist = response.data.isCustomerExists;
+        }
+      } catch (error) {
+        console.log("Some Error Occured");
+      }
+    },
+    [apiBody.cust_number]
+  );
+  const checkInvoiceAndDocId = useCallback(
+    async ({ doc_id, invoice_id }) => {
+      try {
+        const response = await API.get(
+          `CheckInvoiceidAndDocId?doc_id="${doc_id}"&invoice_id="${invoice_id}"`
+        );
+        if (response.status === 200) {
+          isInvoiceIdorDocIdExist = response.data.isInvoiceOrDocExist;
+        }
+      } catch (error) {
+        console.log("Some Error Occured");
+      }
+    },
+    [apiBody.doc_id, apiBody.invoice_id]
+  );
+  const addFunction = useCallback(async () => {
     console.log("addfunction");
+    const {
+      business_code,
+      cust_number,
+      clear_date,
+      business_year,
+      doc_id,
+      posting_date,
+      document_create_date,
+      due_in_date,
+      invoice_currency,
+      document_type,
+      posting_id,
+      total_open_amount,
+      baseline_create_date,
+      cust_payment_terms,
+      invoice_id,
+    } = apiBody;
+
+    console.log(apiBody);
+    await checkBusinessCode({ business_code });
+    await checkCustomerNumber({ cust_number });
+    await checkInvoiceAndDocId({ doc_id, invoice_id });
+    console.log(
+      isBusinessCodeExist,
+      isCustomerNumberExist,
+      isInvoiceIdorDocIdExist
+    );
+    if (isBusinessCodeExist !== 1) {
+      alert(
+        "Entered Business Code donot Exist. Please verify your Business Code"
+      );
+    } else if (isCustomerNumberExist !== 1) {
+      alert(
+        "Entered Customer Number donot Exist. Please verify your Customer Number"
+      );
+    } else if (isInvoiceIdorDocIdExist === 1) {
+      alert(
+        "Entered Invoice Id or Document Id already Exist. Please provide unique entry to these fields"
+      );
+    } else {
+      API.post(`AddInvoiceEntry`, {
+        business_code,
+        cust_number,
+        clear_date,
+        business_year,
+        doc_id,
+        posting_date,
+        document_create_date,
+        due_in_date,
+        invoice_currency,
+        document_type,
+        posting_id,
+        total_open_amount,
+        baseline_create_date,
+        cust_payment_terms,
+        invoice_id,
+      })
+        .then((response) => {
+          response.status === 200 && alert("Data Added Successfully");
+        })
+        .catch((error) => console.log("Some Error Occured"));
+    }
   }, [apiBody]);
 
   const editFunction = useCallback(() => {
@@ -103,13 +253,20 @@ function DialogComponent({ dialogName, dialogElement }) {
                     <DatePicker
                       label={element.field}
                       name={element.name}
+                      value={apiBody[element.name]}
+                      onChange={(newValue) =>
+                        setapiBody((previousState) => ({
+                          ...previousState,
+                          [element.name]: formatDate(newValue),
+                        }))
+                      }
                       views={["year", "month", "day"]}
                       renderInput={(params) => (
                         <TextField
+                          label={element.field}
                           name={element.name}
                           InputProps={{ disableUnderline: true }}
                           required
-                          value={apiBody[element.name]}
                           {...params}
                           variant="standard"
                           sx={{
